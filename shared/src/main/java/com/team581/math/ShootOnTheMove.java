16cf3e367630d2485c221455b2622a06e3c95075
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2026-02-08 14:21:26 -0800

    Clean up code

--- shared/src/main/java/com/team581/math/ShootOnTheMove.java
+++ shared/src/main/java/com/team581/math/ShootOnTheMove.java
@@ -20,23 +20,21 @@ public class ShootOnTheMove {
     var result = target;
 
     for (int i = 0; i < MAX_ITERATIONS; i++) {
-      double tof = distanceToTimeOfFlight.get(robot.getDistance(result));
+      double tof = distanceToTimeOfFlight.get(robot.getDistance(target));
 
       // 1. Get the direction from robot to target as a Rotation2d
       Rotation2d angleToTarget = target.minus(robot).getAngle();
 
       // 2. Extract the radial velocity using trigonometry
-      // This replaces the manual Dot Product math
-      double radialVelocityMag =
+      double radialVelocityMagnitude =
           (robotVelocity.vxMetersPerSecond * angleToTarget.getCos())
               + (robotVelocity.vyMetersPerSecond * angleToTarget.getSin());
 
       // 3. Create the offset vector and subtract it
-      Translation2d offset = new Translation2d(radialVelocityMag * tof, angleToTarget);
+      Translation2d offset = new Translation2d(radialVelocityMagnitude * tof, angleToTarget);
       result = target.minus(offset);
     }
-    DogLog.log(
-        "ShootOnTheMove/TargetRadial", new Pose2d(target.getX(), target.getY(), target.getAngle()));
+    DogLog.log("ShootOnTheMove/RadialVelocityCompensatedGoal", new Pose2d(result, result.getAngle()));
     return result;
   }
 
@@ -45,26 +43,23 @@ public class ShootOnTheMove {
     var result = target;
 
     for (int i = 0; i < MAX_ITERATIONS; i++) {
-      double tof = distanceToTimeOfFlight.get(robot.getDistance(result));
+      double tof = distanceToTimeOfFlight.get(robot.getDistance(target));
 
       // 1. Get the radial vector (robot to target)
-      Translation2d radialDir = target.minus(robot).div(robot.getDistance(target));
+      Translation2d radialDirection = target.minus(robot).div(robot.getDistance(target));
 
       // 2. Get the tangential vector (rotate radial by 90 degrees)
-      Translation2d tangentialDir = new Translation2d(-radialDir.getY(), radialDir.getX());
+      Translation2d tangentialDirection = new Translation2d(-radialDirection.getY(), radialDirection.getX());
 
       // 3. Project robot velocity onto the tangential vector
-      double tangentialVelocityMag =
-          (robotVelocity.vxMetersPerSecond * tangentialDir.getX())
-              + (robotVelocity.vyMetersPerSecond * tangentialDir.getY());
+      double tangentialVelocityMagnitude =
+          (robotVelocity.vxMetersPerSecond * tangentialDirection.getX())
+              + (robotVelocity.vyMetersPerSecond * tangentialDirection.getY());
 
       // 4. Offset target sideways
-      result = target.minus(tangentialDir.times(tangentialVelocityMag * tof));
+      result = target.minus(tangentialDirection.times(tangentialVelocityMagnitude * tof));
     }
-    DogLog.log(
-        "ShootOnTheMove/TargetTangential",
-        new Pose2d(target.getX(), target.getY(), target.getAngle()));
-
+    DogLog.log("ShootOnTheMove/TangentialVelocityCompensatedGoal", new Pose2d(result, result.getAngle()));
     return result;
   }
 
