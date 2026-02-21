ad6707f1d45bff085b4a8b42eaecd994829b5a55
Simon <87281874+simonp17@users.noreply.github.com>
2026-02-20 21:52:53 -0800

    Only override current turret angle in collect inputs

--- comp-bot/src/main/java/frc/robot/turret/Turret.java
+++ comp-bot/src/main/java/frc/robot/turret/Turret.java
@@ -72,22 +72,25 @@ public class Turret extends StateMachineSubsystem<TurretState> {
 
   @Override
   protected void collectInputs() {
-    currentAngle = Units.rotationsToDegrees(motor.getPosition().getValueAsDouble());
+    currentAngle =
+        getState() == TurretState.STUCK
+            ? stuckAngle
+            : Units.rotationsToDegrees(motor.getPosition().getValueAsDouble());
+
     velocity = Units.rotationsToDegrees(motor.getVelocity().getValueAsDouble());
     voltage = motor.getMotorVoltage().getValueAsDouble();
     statorCurrent = motor.getStatorCurrent().getValueAsDouble();
 
     // Predict the turret's current angle to account for sensor latency
     double latencyCompensatedAngle =
-        Units.rotationsToDegrees(
-            BaseStatusSignal.getLatencyCompensatedValueAsDouble(
-                motor.getPosition(), motor.getVelocity()));
+        getState() == TurretState.STUCK
+            ? stuckAngle
+            : Units.rotationsToDegrees(
+                BaseStatusSignal.getLatencyCompensatedValueAsDouble(
+                    motor.getPosition(), motor.getVelocity()));
 
     // Add the predicted angle to the vision buffer at the current timestamp
-    vision.addTurretObservation(
-        Timer.getFPGATimestamp(),
-        getState() == TurretState.STUCK ? stuckAngle : latencyCompensatedAngle,
-        velocity);
+    vision.addTurretObservation(Timer.getFPGATimestamp(), latencyCompensatedAngle, velocity);
 
     DogLog.log("Turret/Angle", currentAngle);
     DogLog.log("Turret/Motor/LatencyCompensatedAngle", latencyCompensatedAngle);
